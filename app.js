b8e21a3b092c13c7359ba2a72054957403f130a5
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-06 12:11:58 +0530

    installed ejs package

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const path = require("path");
+
 const express = require("express");
 
-const authRoutes = require('./routes/auth.routes')
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.use(authRoutes)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(authRoutes);
 
 app.listen(3000);
