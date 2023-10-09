71bdf9f17a9dec3ecf1527e97730e61d3b74eb0a
swapnil2072 <swapnilshinde909693@gmail.com>
2023-10-09 20:55:02 +0530

    finish with payment gateway

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,11 +1,14 @@
-const Order = require('../models/order.model');
-const User = require('../models/user.model');
+const stripe = require("stripe")(
+  "sk_test_51Nys2ZSEVBQy9Gv71n9neBXPyHtFWLUwdO83kHzlPvAdxbyEcxEucQ4Yssuy9H4sXaDT6rXs35EvvR5Edg6uqo4p00UcAvG27e",
+);
+const Order = require("../models/order.model");
+const User = require("../models/user.model");
 
 async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
-    res.render('customer/orders/all-orders', {
-      orders: orders
+    res.render("customer/orders/all-orders", {
+      orders: orders,
     });
   } catch (error) {
     next(error);
@@ -33,10 +36,39 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: cart.items.map(function(item) {
+      return  {
+        price_data: {
+          currency: 'inr',
+          product_data: {
+            name: item.product.title
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100
+        },
+        quantity: item.quantity,
+      }
+    }),
+    mode: 'payment',
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+  res.render('customer/orders/failure');
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
 };
